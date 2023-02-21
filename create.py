11b62512d0b8f3cef5da9dbb15744750f78d1cdd
Felipe Silva <ffelipe.ssilva02@gmail.com>
2023-02-21 20:15:12 -0300

    added user confirmation

--- create.py
+++ create.py
@@ -7,14 +7,24 @@ conexao = sqlcon.connect(
     database='bdinfosecurity',
 )
 cursor = conexao.cursor()
-produto ='pedra redonda'
-valor = 2
-comando=f"insert into bdinfosecurity.sell (sellprod, sellprice) values ('{produto}', {valor})"
-print("print")
-print(comando)
-cursor.execute(comando)
-conexao.commit()
-resultado=cursor.fetchall()
-print(resultado)
-cursor.close()
-conexao.close()
+name= input("Enter username:")
+mail= input("Enter email:")
+passw= input("Enter password:")
+consent= input("By typing OK button, you are creating an account, and agree to Terms of Service and Privacy Policy:")
+
+while consent !="OK":
+    if consent=="termos":
+        print("esses são os termos de serviço")
+    elif consent=="privacy":
+        print("essa é a politica de privacidade")
+    consent= input("By typing OK button, you are creating an account, and agree to Terms of Service and Privacy Policy:")
+if consent == "OK":
+    comando=f"insert into bdinfosecurity.user (username, usermail, userpassword) values ('{name}', '{mail}', '{passw}')"
+    print("print")
+    print(comando)
+    cursor.execute(comando)
+    conexao.commit()
+    resultado=cursor.fetchall()
+    print(resultado)
+    cursor.close()
+    conexao.close()
