f2844593ec06ef6befd6990239e3734b89de6c1f
Felipe Silva <ffelipe.ssilva02@gmail.com>
2023-03-04 13:29:15 -0300

    tentativa de criar log

--- delete.py
+++ delete.py
@@ -6,12 +6,35 @@ conexao = sqlcon.connect(
     password='1234',
     database='bdinfosecurity',
 )
-cursor = conexao.cursor()
-produto ='pedra redonda'
-valor=600000
-comando=f"delete from bdinfosecurity.sell where sellprod = '{produto}'"
-cursor.execute(comando)
-conexao.commit()
-resultado=cursor.fetchall()
-cursor.close()
-conexao.close()
+
+
+confirm= input("Tem certeza que deseja excluir esse dado? ele não poderá ser restaurado... ")
+if(confirm=="sim"):
+    try:
+        name= input("Enter username:")
+        passw= input("Enter password:")
+        validate = conexao.cursor()
+        comando=f"select * from bdinfosecurity.user where user.username = '{name}' and user.userpassword = '{passw}'"
+        validate.execute(comando)
+        resultado=validate.fetchall()
+        validate.close()
+        print("resultado")
+        print(resultado)
+        if(resultado !="[]"):
+            deleter = conexao.cursor()
+            ID = input("ID do usuario a ser excluido:")
+            comando=f"delete from bdinfosecurity.user where user.userid = '{ID}'"
+            deleter.execute(comando)
+            conexao.commit()
+            resultado=deleter.fetchall()
+            deleter.close()
+            conexao.close()
+
+            print("dado excluido")
+        else:
+            print("acesso negado")
+    except:
+        validation = None
+        print("acesso negado")
+else:
+    print("processo encerrado")
--- read.py
+++ read.py
@@ -6,10 +6,41 @@ conexao = sqlcon.connect(
     password='1234',
     database='bdinfosecurity',
 )
-cursor = conexao.cursor()
-comando='select * from bdinfosecurity.sell'
-cursor.execute(comando)
-resultado=cursor.fetchall()
-print(resultado)
-cursor.close()
-conexao.close()
+
+try:
+    name= input("Enter username:")
+    passw= input("Enter password:")
+    validate = conexao.cursor()
+    comando=f"select user.userid from bdinfosecurity.user where user.username = '{name}' and user.userpassword = '{passw}'"
+    print(comando)
+    validate.execute(comando)
+    idsession=validate.fetchone()
+    print("ID DA SESSÃO")
+    print(idsession)
+    if idsession != None:
+        validation = "ok"
+        print("login concluido")
+    else:
+        validation = None
+        print("acesso negado")
+    validate.close()
+except:
+    validation = None
+    print("acesso negado")
+
+if(validation=="ok"):
+    cursor = conexao.cursor()
+    comando='select user.usermail from bdinfosecurity.user'
+    cursor.execute(comando)
+    resultado=cursor.fetchall()
+    print("Emails de usuarios devedores. Dados devem ser utilizados apenas para cobrança")
+    print(resultado)
+    cursor.close()
+
+    log = conexao.cursor()
+    comando=f"insert into bdinfosecurity.accesslog (iduseracces, context) values ('{idsession}', 'combrança por email')"
+    print("print")
+    print(comando)
+    log.execute(comando)
+    conexao.commit()
+    log.close()
