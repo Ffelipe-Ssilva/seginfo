a99ed85d07fa008928c94e8ea4b9391b704f46a0
brennorichard <brennotrich2002@gmail.com>
2023-03-14 10:28:10 -0300

    Inserindo arquivo de importação de dados

---
+++ importdata.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from pymongo import MongoClient
+
+# Conectar ao banco de dados MySQL
+conexao = sqlcon.connect(
+    host='localhost',
+    user='root',
+    password='1234',
+    database='bdinfosecurity',
+)
+
+# Conectar ao banco de dados MongoDB
+mongo_client = MongoClient("mongodb+srv://admin-fatec:adminadmin@clustersi.4kdc2kp.mongodb.net/?retryWrites=true&w=majority")
+mongo_db = mongo_client['Login-Usuarios']
+mongo_collection = mongo_db['usuarios']
+
+
+# Recuperar os dados do MySQL
+mysql_cursor = mysql_conn.cursor()
+mysql_cursor.execute('SELECT * FROM tabela')
+mysql_data = mysql_cursor.fetchall()
+
+# Inserir os dados do MySQL no MongoDB
+for row in mysql_data:
+    data = {
+        'campo1': row[0],
+        'campo2': row[1],
+        'campo3': row[2],
+        # ...
+    }
+    mongo_collection.insert_one(data)
