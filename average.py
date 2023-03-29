bdcf29a457746d807f349431e81684ea711c1ee1
Felipe Silva <ffelipe.ssilva02@gmail.com>
2023-03-28 22:46:05 -0300

    opt in out

---
+++ average.py
@@ -0,0 +1,62 @@
+import mysql.connector as sqlcon
+import getpass
+
+conexao = sqlcon.connect(
+    host='localhost',
+    user='root',
+    password='1234',
+    database='bdinfosecurity',
+)
+
+try:
+    name= input("Enter username:")
+    passw= getpass.getpass(prompt="Enter password:")
+    validate = conexao.cursor()
+    comando=f"select user.userid from bdinfosecurity.user where user.username = '{name}' and user.userpassword = '{passw}' and user.userrole='admin'"
+
+    validate.execute(comando)
+    queryresult=validate.fetchone()
+    for row in queryresult:
+        idsession=row
+
+    if idsession != None:
+        validation = "ok"
+        print("login concluido")
+    else:
+        validation = None
+        print("acesso negado")
+    validate.close()
+except:
+    validation = None
+    print("acesso negado")
+
+if(validation=="ok"):
+    cursor = conexao.cursor()
+    comando='select user.userethnicity,count(user.userethnicity) as etnia from bdinfosecurity.user where user.userethnicity is not null group by user.userethnicity'
+    cursor.execute(comando)
+    resultado=cursor.fetchall()
+    print("Contagem de atnias dos usuários")
+    print(resultado)
+    cursor.close()
+
+    cursor = conexao.cursor()
+    comando='select user.userage,count(user.userage) as etnia from bdinfosecurity.user where user.userage is not null group by user.userage'
+    cursor.execute(comando)
+    resultado=cursor.fetchall()
+    print("Contagem de idades dos usuários")
+    print(resultado)
+    cursor.close()
+
+    cursor = conexao.cursor()
+    comando='select user.usergender,count(user.usergender) as etnia from bdinfosecurity.user where user.usergender is not null group by user.usergender'
+    cursor.execute(comando)
+    resultado=cursor.fetchall()
+    print("Contagem de genero dos usuários")
+    print(resultado)
+    cursor.close()
+
+    log = conexao.cursor()
+    comando=f"insert into bdinfosecurity.accesslog (iduseracces, context) values ({idsession}, 'geração de indicadores')"
+    log.execute(comando)
+    conexao.commit()
+    log.close()
--- create.py
+++ create.py
@@ -12,23 +12,37 @@ cursor = conexao.cursor()
 name= input("Enter username:")
 mail= input("Enter email:")
 passw= getpass.getpass(prompt="Enter password:")
-consent= input("By typing OK button, you are creating an account, and agree to Terms of Service and Privacy Policy:")
 
+#consentimento dos dados sensíveis
+sensible= input("Nosso sistema conta com indicadores que utilizam dados que podem ser considerados sensíveis para alguns usuários. Voce consente fornecer dados como etnia, idade e genero? Essas informações são completamente opcionais e não prejudicarão sua experiência com o sistema.")
+if sensible=="sim":
+    print("Okay, por favor preencha as seguintes informações")
+    eth= input("Enter ethnicity:")
+    gen= input("Enter gender:")
+    age= input("Enter age:")
+elif sensible=="não":
+    print("Okay, não utilizaremos informações sensíveis")
+
+consent= input("By typing OK button, you are creating an account, and agree to Terms of Service and Privacy Policy:")
 while consent !="OK":
     if consent=="termos":
         print("esses são os termos de serviço")
     elif consent=="privacy":
         print("essa é a politica de privacidade")
     consent= input("By typing OK button, you are creating an account, and agree to Terms of Service and Privacy Policy:")
+#if consent == "OK":
+#    ads=input("deseja utilizar seus dados para customizar sua experiencia?")
+#    while ads !="sim" or ads !="não":
+#        if ads=="sim":
+#            print("seus anuncios serão customizado de acordo com seus gostos")
+#        elif ads=="não":
+#            print("seus anuncios serão gerados aleatoriamente")
+#        ads= input("")
 if consent == "OK":
-    while ads !="sim" or ads !="não":
-        if ads=="sim":
-            print("seus anuncios serão customizado de acordo com seus gostos")
-        elif ads=="não":
-            print("seus anuncios serão gerados aleatoriamente")
-        ads= input("")
-if consent == "OK":
-        comando=f"insert into bdinfosecurity.user (username, usermail, userpassword, userrole) values ('{name}', '{mail}', '{passw}', 'user')"
+        if sensible=="sim":
+            comando=f"insert into bdinfosecurity.user (username, usermail, userpassword, userrole, usergender, userethnicity, userage) values ('{name}', '{mail}', '{passw}', 'user', '{gen}', '{eth}', {age})"
+        elif sensible=="não":
+             comando=f"insert into bdinfosecurity.user (username, usermail, userpassword, userrole) values ('{name}', '{mail}', '{passw}', 'user')"    
 
         cursor.execute(comando)
         conexao.commit()
