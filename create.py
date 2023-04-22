c06d94751a855562d67087fae43a7a1dbb41140d
Felipe Silva <ffelipe.ssilva02@gmail.com>
2023-04-21 23:37:12 -0300

    atualização dos termos

--- create.py
+++ create.py
@@ -10,29 +10,31 @@ name= input("Enter username:")
 mail= input("Enter email:")
 passw= getpass.getpass(prompt="Enter password:")
 
-#consentimento dos dados sensíveis
-sensible= input("Nosso sistema conta com indicadores que utilizam dados que podem ser considerados sensíveis para alguns usuários. Voce consente fornecer dados como etnia, idade e genero? Essas informações são completamente opcionais e não prejudicarão sua experiência com o sistema.")
-if sensible=="sim":
-    print("Okay, por favor preencha as seguintes informações")
-    eth= input("Enter ethnicity:")
-    gen= input("Enter gender:")
-    age= input("Enter age:")
-elif sensible=="não":
-    print("Okay, não utilizaremos informações sensíveis")
 
-ads= input("Deseja utilizar informações das suas comprar para personalizar seus anuncios? (Isso não influenciara na aparição de anúncios, apenas seu conteúdo)")
-if ads=="sim":
-    userads=True
-else:
-    userads=False
-
-consent= input("By typing OK button, you are creating an account, and agree to Terms of Service and Privacy Policy:")
+consent= input("Termos de politica e privacidade versão 1")
 while consent !="OK":
-    if consent=="termos":
-        print("esses são os termos de serviço")
-    elif consent=="privacy":
-        print("essa é a politica de privacidade")
+    if consent=="sensivel":
+        sensible= input("Nosso sistema conta com indicadores que utilizam dados sensíveis. Voce consente fornecer dados como etnia, idade e genero? Essas informações são completamente opcionais e não prejudicarão sua experiência com o sistema.")
+        if sensible=="sim":
+            print("Okay, por favor preencha as seguintes informações")
+            eth= input("Enter ethnicity:")
+            gen= input("Enter gender:")
+            age= input("Enter age:")
+        elif sensible=="não":
+            print("Okay, não utilizaremos informações sensíveis")
+    elif consent=="adds":
+        ads= input("Deseja utilizar informações das suas comprar para personalizar seus anuncios? (Isso não influenciara na aparição de anúncios, apenas seu conteúdo)")
+        if ads=="sim":
+            userads=True
+        else:
+            userads=False
     consent= input("By typing OK button, you are creating an account, and agree to Terms of Service and Privacy Policy:")
+
+#consentimento dos dados sensíveis
+
+
+
+
 #if consent == "OK":
 #    ads=input("deseja utilizar seus dados para customizar sua experiencia?")
 #    while ads !="sim" or ads !="não":
